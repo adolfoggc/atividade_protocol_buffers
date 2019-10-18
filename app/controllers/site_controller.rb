9d186e13d1086c297eaf08ff5e8dbf19331cca66
Carlos Adolfo <adolfo.ggc@gmail.com>
2019-10-18 11:50:15 -0300

    olha, deu bom

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,13 @@
 class SiteController < ApplicationController
   def room_data
+  	@rooms = RoomStatus.order(id: :desc)
   end
+
+  def get_info
+  	room = RoomStatus.new
+  	room.ac_temperature = params[:info].to_i
+  	room.save
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'site/room_data'
   resources :room_statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/insert/:info', to: 'site#get_info', as: "get_info"
 end
