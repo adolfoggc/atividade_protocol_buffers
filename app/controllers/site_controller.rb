2a8357921cccd37f8d15280fb2075307dcd4ef1a
Carlos Adolfo <adolfo.ggc@gmail.com>
2019-10-22 00:15:17 -0300

    problemas em enviar via socket

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,6 @@
 class SiteController < ApplicationController
   include ApplicationHelper
+  require 'socket'
 
   def room_data
   	@lights = Light.first
@@ -22,5 +23,55 @@ class SiteController < ApplicationController
     #obj = tv_encode(:REQ, 95, true)
   	obj = termo_encode(:REQ, 35)
     redirect_to get_info_path(info: obj )
+  end
+
+  #gets e sets
+  def get_temp
+    termo = Thermo.new
+    termo.to_ruby(params[:info])
+    if (!termo.temperature.nil?)
+      obj.save
+    end
+  end
+
+  def set_temp
+    termo = termo_encode(params)
+    ###
+  end
+
+  def get_tv
+    tv = Tv.first
+    tv.to_ruby(params[:info])
+    if (!tv.status.nil?)
+      tv.save
+    end
+  end
+
+  def set_tv
+    tv = tv_encode(params)
+    ###
+  end
+
+  def get_lamp
+    lamp = Light.first
+    lamp.to_ruby(params[:info])
+    if (!lamp.status.nil?)
+      lamp.save
+    end
+  end
+
+  def set_lamp
+    #lamp = lamp_encode(params)
+    #link_to 'test', root_url(:port => 80, :only_path => false)
+    ###
+
+    # s = TCPSocket.open('localhost', 3004)
+    # puts "\UPTIME"
+    # s.close
+    s = TCPSocket.new 'localhost', 8080
+
+    s.puts('UPTIME'.to_s)
+    s.close
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/insert/:info', to: 'site#get_info', as: "get_info"
   get '/lamp', to: 'site#gera_info', as: 'gera_info'
+
+  get '/g_lamp', to: 'site#get_lamp', as: 'get_lamp'
+  get '/s_lamp', to: 'site#set_lamp', as: 'set_lamp'
+  get '/g_termo', to: 'site#get_termo', as: 'get_termo'
+  get '/s_termo', to: 'site#set_termo', as: 'set_termo'
+  get '/g_tv', to: 'site#get_tv', as: 'get_tv'
+  get '/s_tv', to: 'site#set_tv', as: 'set_tv'
 end
